4c70c0b87d9178ef95572ea7db79d016de2dd0a3
Nabilah Erfariani <68337892+nabilaherfa@users.noreply.github.com>
2020-11-09 21:14:50 +0700

    Update countDigit.pl

--- countDigit.pl
+++ countDigit.pl
@@ -1,6 +1,6 @@
 /* Program Count Digit */
 /* countDigit(A,X) : X merupakan banyak digit dari A /
-/* Nabilah Erfariani(13519181) */
+
 countDigit(0, 1).
 countDigit(X, 1) :- X < 10. 
 countDigit(X, Y) :- Z is (X / 10), countDigit(Z, A), Y is (A + 1).
