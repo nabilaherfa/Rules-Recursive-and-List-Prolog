3d97ab7c425ed0afcbadb62c379ca78ea3d5e87b
Nabilah Erfariani <68337892+nabilaherfa@users.noreply.github.com>
2020-11-09 21:12:57 +0700

    Update faktorial.pl

--- faktorial.pl
+++ faktorial.pl
@@ -1,5 +1,5 @@
 /* Program Faktorial */
 /* X merupakan hasil perkalian bilangan bulat dari 1 hingga N dengan ketentuan N >= 1. /
-/* Nabilah Erfariani(13519181) */
+
 faktorial(0,1).
 faktorial(X,Y) :- X > 0, Z is X - 1, faktorial(Z,A), Y is X * A.
