6b4178f46a55da89da3701ac66ec56632a9f1959
Nabilah Erfariani <68337892+nabilaherfa@users.noreply.github.com>
2020-11-09 21:16:59 +0700

    Update power.pl

--- power.pl
+++ power.pl
@@ -1,4 +1,4 @@
 /* Program Power*/
 /*power(A,B,X) : X adalah hasil A dipangkatkan dengan B*/     
 power(X,0,1).
-power(X,Y,Z) :- Z is X**Y. 
+power(X,Y,Z) :- Y>0, Y1 is Y-1, power(X,Y1,Z1), Z is (Z1*X).
